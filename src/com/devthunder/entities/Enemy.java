e653da74e3ce8086a084261c426e7d465f4c422b
Gabriel Bento Lisboa <gabrielbento.rx@outlook.com>
2019-07-23 13:45:55 -0300

    Enemy IA and colision

--- src/com/devthunder/entities/Enemy.java
+++ src/com/devthunder/entities/Enemy.java
@@ -1,9 +1,29 @@
 package com.devthunder.entities;
 
+import com.devthunder.main.Game;
+import com.devthunder.world.World;
+
 import java.awt.image.BufferedImage;
 
 public class Enemy extends Entity {
+
+    private double speed = 0.2;
+
     public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
         super(x, y, width, height, sprite);
+    }
+
+    public void tick() {
+        if (x < Game.player.getX() && World.isFree((int) (x+speed), this.getY())) {
+            x+=speed;
+        } else if (x > Game.player.getX() && World.isFree((int) (x-speed), this.getY())) {
+            x-=speed;
+        }
+
+        if (y < Game.player.getY() && World.isFree(this.getX(), (int)(y+speed))) {
+            y+=speed;
+        } else if (y > Game.player.getY() && World.isFree(this.getX(), (int)(y-speed))) {
+            y-=speed;
+        }
     }
 }
--- src/com/devthunder/main/Game.java
+++ src/com/devthunder/main/Game.java
@@ -116,6 +116,7 @@ public class Game extends Canvas implements Runnable, KeyListener {
         double delta = 0;
         int frames = 0;
         double timer = System.currentTimeMillis();
+        requestFocus();
 
         while (isRunning) {
             long now = System.nanoTime();
